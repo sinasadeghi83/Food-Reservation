1dacf838143f62ca3ae706adebad6900ac5cde6c
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-01-30 21:08:16 +0330

    Removing build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sinasadeghi/Programming/Clang/Project2/Food-Reservation
-# Build directory: /home/sinasadeghi/Programming/Clang/Project2/Food-Reservation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
